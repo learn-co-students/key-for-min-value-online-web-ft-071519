01c7b3bf461e23c85817f306c84e4d910c3dc3db
Emily Lillie <emilylillie3@gmail.com>
2019-07-19 18:08:18 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,25 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+name_hash = {:blake => 500, :ashley => 2, :adam => 1}
 
+def key_for_min_value(name_hash)
+   current_minimum_val = nil 
+  current_key_assoc_w_min_val = nil 
+ 
+  name_hash.each do |key, value|
+  
+    if current_minimum_val == nil 
+      current_minimum_val = value 
+      current_key_assoc_w_min_val = key 
+    else 
+    
+      if current_minimum_val > value 
+        current_minimum_val = value
+        current_key_assoc_w_min_val = key 
+      end
+    end
+  end 
+  current_key_assoc_w_min_val 
 end
+#current minval = 0. compare that to first value. if that value < currentmin, it becomes current min. loop to end. print key assoc with currentmin
